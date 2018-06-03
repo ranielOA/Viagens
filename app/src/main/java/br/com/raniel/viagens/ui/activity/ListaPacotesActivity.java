f029afd8af64fbc89f063da22f3b834116cb14ae
Raniel <r.a_tw@hotmail.com>
2018-06-03 13:58:52 -0300

    implementando lista personalizada

--- app/src/main/java/br/com/raniel/viagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/raniel/viagens/ui/activity/ListaPacotesActivity.java
@@ -2,8 +2,14 @@ package br.com.raniel.viagens.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.raniel.viagens.R;
+import br.com.raniel.viagens.dao.PacoteDAO;
+import br.com.raniel.viagens.model.Pacote;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -11,5 +17,11 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        ListView listaPacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaPacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
